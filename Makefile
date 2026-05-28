04379ca8338a07f37db08f0f1ca0a3f4d4d17407
Vasya <vasya130393@yandex.ru>
2026-05-28 18:36:49 +0300

    added view_count and relations

--- Makefile
+++ Makefile
@@ -3,3 +3,12 @@ run:
 
 lint:
 	uv run pre-commit run --all-files
+
+migrations:
+	uv run manage.py makemigrations
+
+migrate:
+	uv run manage.py migrate
+
+superuser:
+	uv run manage.py createsuperuser
--- blog_app/models.py
+++ blog_app/models.py
@@ -14,3 +14,14 @@ class Post(models.Model):
     author = models.ForeignKey(to='auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='posts')
+
+
+    def increase_views_count(self):
+        self.views_count += 1
+        self.save()
+
+
+    def __str__(self):
+        return self.title
