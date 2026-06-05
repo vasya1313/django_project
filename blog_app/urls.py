862d53cda16fe2b02e749553d500305562f22c08
Vasya <vasya130393@yandex.ru>
2026-06-05 18:05:37 +0300

    added categories views and updated posts to use slug

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,5 +7,7 @@ app_name = 'blog_app'
 urlpatterns = [
     path("", views.index, name="index_page"), # blog:index_page будет доступно в шаблонах
     path("posts/", views.posts_list, name="posts_list"),
-    path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
+    path("posts/<slug:post_slug>/", views.post_detail, name="post_detail"),
+    path("categories/", views.categories_list, name="categories_list"),
+    path("categories/<slug:category_slug>/", views.category_detail, name="category_detail"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-from blog_app.models import Post
+from blog_app.models import Post, Category
 
 
 def index(request):
@@ -13,13 +13,13 @@ def posts_list(request):
 
     content = "<h1>Опубликованные статьи</h1><br><br>"
     for post in posts:
-        content += f"<a href='/posts/{post.id}/'>{post.title}</a> ({post.created_at})<br>"
+        content += f"<a href='/posts/{post.slug}/'>{post.title}</a> ({post.created_at:%Y-%m-%d})<br>"
 
     return HttpResponse(content)
 
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+def post_detail(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
 
     content = f'''
         <h1>{post.title}</h1>
@@ -29,4 +29,26 @@ def post_detail(request, post_id):
         <a href="/posts/">Назад к статьям</a>
         '''
 
+    return HttpResponse(content)
+
+
+def categories_list(request):
+    categories = Category.objects.all()
+
+    content = "<h1>Категории постов:</h1><ul>"
+    for category in categories:
+        content += f"<li><a href='/categories/{category.slug}/'>{category.title}</a></li>"
+    content += "</ul>"
+
+    return HttpResponse(content)
+
+def category_detail(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = Post.objects.filter(category=category, publishes=True)
+
+    content = "<h1>Опубликованные статьи</h1><br><br>"
+    for post in posts:
+        content += f"<a href='/posts/{post.slug}/'>{post.title}</a> ({post.created_at:%Y-%m-%d})<br>"
+    content += "<hr>"
+    content += "<a href='/categories/'>Назад к категориям</a>"
     return HttpResponse(content)
