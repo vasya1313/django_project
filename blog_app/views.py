8ac943087100e76bac658b7f919e59575192a897
Vasya <vasya130393@yandex.ru>
2026-06-18 18:12:48 +0300

    Added Class_Post in view.py

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from slugify import slugify
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from blog_app.models import Post, Category
 from blog_app.forms import PostForm, SearchForm, CategoryForm
@@ -20,20 +22,34 @@ def index(request):
     return render(request, 'blog/index.html', context)
 
 
-def posts_list(request):
-    posts = Post.objects.filter(publishes=True)
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/posts_list.html', context)
+# def posts_list(request):
+#     posts = Post.objects.filter(publishes=True)
+#     context = {
+#         'posts': posts
+#     }
+#     return render(request, 'blog/posts_list.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/posts_list.html"
+    context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Post.objects.filter(publishes=True)
 
 
-def post_detail(request, post_slug):
-    post = get_object_or_404(Post, slug=post_slug, publishes=True)
-    # В будущем здесь можно вызывать post.increase_views_count()
-    context = {
-        'post': post}
-    return render(request, 'blog/post_detail.html', context)
+# def post_detail(request, post_slug):
+#     post = get_object_or_404(Post, slug=post_slug, publishes=True)
+#     # В будущем здесь можно вызывать post.increase_views_count()
+#     context = {
+#         'post': post}
+#     return render(request, 'blog/post_detail.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    slug_url_kwarg = 'post_slug'
 
 
 def categories_list(request):
@@ -56,17 +72,32 @@ def category_detail(request, category_slug):
       return render(request, 'blog/category_detail.html', context)
 
 
-def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(data=request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.slug = slugify(post.title)
-            post.save()
-            return redirect('blog:index_page')
-    else:
-        form = PostForm()
-    return render(request, "blog/post_create.html", context={'form': form})
+# def post_create(request):
+#     if request.method == 'POST':
+#         form = PostForm(data=request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.slug = slugify(post.title)
+#             post.save()
+#             return redirect('blog:index_page')
+#     else:
+#         form = PostForm()
+#     return render(request, "blog/post_create.html", context={'form': form})
+
+class PostFormBase():
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:index_page')
+
+class PostCreateView(PostFormBase, CreateView):
+    # model = Post
+    # form_class = PostForm
+    template_name = 'blog/post_create.html'
+    # success_url = reverse_lazy('blog:index_page')
+
+    def form_valid(self, form):
+        form.instance.slug = slugify(form.instance.title)
+        return super().form_valid(form)
 
 
 def category_create(request):
@@ -82,10 +113,23 @@ def category_create(request):
     return render(request, "blog/category_create.html", context={'form': form})
 
 
-def post_edit(request, post_slug):
-    post = get_object_or_404(Post, slug=post_slug)
-    form = PostForm(data=request.POST or None, instance=post)
-    if request.method == 'POST' and form.is_valid():
-        post.save()
-        return redirect('blog:post_detail', post_slug=post.slug)
-    return render(request, "blog/post_edit.html", context={'form': form})
+# def post_edit(request, post_slug):
+#     post = get_object_or_404(Post, slug=post_slug)
+#     form = PostForm(data=request.POST or None, instance=post)
+#     if request.method == 'POST' and form.is_valid():
+#         post.save()
+#         return redirect('blog:post_detail', post_slug=post.slug)
+#     return render(request, "blog/post_edit.html", context={'form': form})
+
+class PostUpdateView(PostFormBase, UpdateView):
+    # model = Post
+    # form_class = PostForm
+    template_name = "blog/post_edit.html"
+    slug_url_kwarg = 'post_slug'
+    # success_url = reverse_lazy('blog:index_page')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('blog:index_page')
+    slug_url_kwarg = 'post_slug'
