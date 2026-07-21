731578f9455576779cd582cf61f95aa4c6874858
Vasya <vasya130393@yandex.ru>
2026-07-21 18:38:15 +0300

    remade tests

--- drf_app/tests/test_PostViewSet.py
+++ drf_app/tests/test_PostViewSet.py
@@ -3,13 +3,16 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from blog_app.models import Post, Category
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class PostAPITests(APITestCase):
 
-
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='password123')
+        self.refresh = RefreshToken.for_user(self.user)
+        self.access_token = str(self.refresh.access_token)
+
         self.category = Category.objects.create(title='Тестовая категория', slug='test-category')
         Post.objects.create(
             title='Тестовый пост',
@@ -18,15 +21,16 @@ class PostAPITests(APITestCase):
             author=self.user
         )
 
+
     def test_get_posts_list(self):
         url = reverse('drf:post-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
-    def test_create_post_authenticated(self):
-        self.client.force_authenticate(user=self.user)
 
+    def test_create_post_authenticated(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
         url = reverse('drf:post-list')
         data = {
             'title': 'Новый пост от авторизованного пользователя',
@@ -38,6 +42,7 @@ class PostAPITests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Post.objects.filter(title=data['title']).exists())
+
 
     def test_create_post_anonymous(self):
         url = reverse('drf:post-list')
