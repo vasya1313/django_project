2d85991630e60632b97025df11ce0ffab50f2620
Vasya <vasya130393@yandex.ru>
2026-07-21 22:39:26 +0300

    Remake test v1.01

--- drf_app/tests/test_PostViewSet.py
+++ drf_app/tests/test_PostViewSet.py
@@ -12,15 +12,6 @@ class PostAPITests(APITestCase):
         self.password = 'password123'
         self.user = User.objects.create_user(username=self.username, password=self.password)
 
-        auth_url = reverse('token_obtain_pair')
-        auth_data = {
-            'username': self.username,
-            'password': self.password
-        }
-        auth_response = self.client.post(auth_url, auth_data, format='json')
-
-        self.access_token = auth_response.data['access']
-
         self.category = Category.objects.create(title='Тестовая категория', slug='test-category')
         Post.objects.create(
             title='Тестовый пост',
@@ -38,15 +29,25 @@ class PostAPITests(APITestCase):
 
 
     def test_create_post_authenticated(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        auth_url = reverse('token_obtain_pair')
+        auth_data = {
+            'username': self.username,
+            'password': self.password
+        }
+        auth_response = self.client.post(auth_url, auth_data, format='json')
+        access_token = auth_response.data['access']
+
+        headers = {
+            'HTTP_AUTHORIZATION': f'Bearer {access_token}'
+        }
+
         url = reverse('drf:post-list')
         data = {
             'title': 'Новый пост от авторизованного пользователя',
             'content': 'Текст нового поста.',
             'category': self.category.pk
         }
-
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, data, format='json', **headers)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Post.objects.filter(title=data['title']).exists())
